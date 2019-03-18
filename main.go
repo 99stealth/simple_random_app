57a1a8fe5c34219e8e219d421f0ca20faa6cdd8b
Roman Banakh <banakh.ri@gmail.com>
2019-03-18 15:02:48 +0200

    Commit with changed health-check

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 	r := mux.NewRouter()
 
 	// Router Handler / Endpoints
-	r.HandleFunc("/healt-check", getHealthStatus).Methods("GET")
+	r.HandleFunc("/healt", getHealthStatus).Methods("GET")
 	r.HandleFunc("/api/random-number", getRandomNumber).Methods("POST")
 
 	fmt.Println("Starting web server on port " + strconv.Itoa(serverPort))
